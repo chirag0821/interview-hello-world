30aa8cf13e2610bf254eb5a3741d55f4f62f35a0
chirag <chiragranpise21@gmail.com>
2026-01-03 01:30:45 +0530

    fix: oidc permissions

--- terraform/main.tf
+++ terraform/main.tf
@@ -128,7 +128,7 @@ resource "aws_iam_role" "github_actions_role" {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
           }
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:chirag0821/interview-hello-world:ref:refs/heads/main"
+            "token.actions.githubusercontent.com:sub" = "repo:chirag0821/interview-hello-world:*"
           }
         }
       }
